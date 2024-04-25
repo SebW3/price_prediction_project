4a5d781c471ff38c54b6cb78495bae4bf2b67ed8
SebW3 <sebwoss@gmail.com>
2024-04-25 23:00:01 +0200

    Trained models on new data

--- machine_learning.py
+++ machine_learning.py
@@ -13,18 +13,12 @@ from sklearn.ensemble import AdaBoostRegressor
 from sklearn.gaussian_process import GaussianProcessRegressor
 
 
-def convert_to_numeric(value):
-    try:
-        return pd.to_numeric(value)
-    except ValueError:
-        return None
-
-
 data = pd.read_csv("car_deatils_for_ml_model.csv", index_col=0)
-data.drop(columns=["name"], inplace=True)
+test = data["selling_price"].copy()
+data.drop(columns=["selling_price"], inplace=True)
 print(data.head())
 
-X_train, X_test, y_train, y_test = train_test_split(data, data, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(data, test, test_size=0.2, random_state=0)
 
 
 
@@ -40,24 +34,23 @@ for model in models:
         print("\n", str(model))
         model.fit(X_train, y_train)
 
-
-        prices_pred = model.predict(X_test)
-        mse = mean_squared_error(X_test, prices_pred)
+        y_pred = model.predict(X_test)
+        mse = mean_squared_error(y_test, y_pred)
         mse_list.append(mse)
         print(f'Błąd średniokwadratowy: {mse}')
-        mae = mean_absolute_error(X_test, prices_pred)
+        mae = mean_absolute_error(y_test, y_pred)
         mae_list.append(mae)
         print(f'Błąd bezwzględny średni: {mae}')
         rmse = np.sqrt(mse)
         rmse_list.append(rmse)
         print(f'Pierwiastek z błędu średniokwadratowego: {rmse}')
-        r2 = r2_score(X_test, prices_pred)
+        r2 = r2_score(y_test, y_pred)
         r2_list.append(r2)
         print(f'Współczynnik determinacji: {r2}')
     except:
         print("error")
 
-lists = [mse_list, mae_list, rmse_list, r2_list]
-for item in lists:
-    for score in item:
-        print(score)
+# lists = [mse_list, mae_list, rmse_list, r2_list]
+# for item in lists:
+#     for score in item:
+#         print(score)
